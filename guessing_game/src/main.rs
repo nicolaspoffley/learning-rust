c2a5cc608d7ba1626135c91bd9808623259dc89f
Nicolas Poffley <nicolas.poffley@gmail.com>
2025-10-05 17:12:55 +0200

    Guessing game.

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,40 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    // Generate a secret number between 1 and 100.
+    // Use thread_rng().gen_range for the current rand API.
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
+
+    println!("Guess the number!");
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a valid number between 1 and 100.");
+                continue;
+            }
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
